3ea70dc78ce8bad3b4664d8b4eb8d0a9a5749d73
Dorian Rina <dorian.rina@gmail.com>
2022-10-29 00:10:07 +0200

    initial commit

---
+++ lib/screens/details/components/product_title_with_image.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/Product.dart';
+
+import 'package:shop_app/constants.dart';
+
+class ProductTitleWithImage extends StatelessWidget {
+  const ProductTitleWithImage({
+    Key key,
+    @required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Aristocratic Hand Bag",
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: kDefaultPaddin),
+          Text(
+            product.title,
+            style: Theme.of(context)
+                .textTheme
+                .headline4
+                .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: <Widget>[
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(text: "Price\n"),
+                    TextSpan(
+                      text: "\$${product.price}",
+                      style: Theme.of(context).textTheme.headline4.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: kDefaultPaddin,
+              ),
+              Expanded(
+                child: Hero(
+                  tag: "${product.id}",
+                  child: Image.asset(
+                    product.image,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
